6c39f15afd7ac479aa4f386cd8d17ab263248e71
e-qui-nox <yamuna.pixie@gmail.com>
2018-07-18 20:30:08 +0530

    Codeforces

---
+++ 123. The sum.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k,t1 = 1,t2 = 1,nextTerm = 0,sum = 0;
+    cin >> k;
+    for (int i = 1; i <= k; ++i)
+    {
+        if(i == 1)
+        {
+            sum+=t1;
+            continue;
+        }
+        if(i == 2)
+        {
+            sum+=t2;
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        sum += nextTerm;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
