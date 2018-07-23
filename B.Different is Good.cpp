3784f3455f4849e63af053afb1ee91341ecddb87
e-qui-nox <yamuna.pixie@gmail.com>
2018-07-23 21:18:10 +0530

    Codeforces

---
+++ B.Different is Good.cpp
@@ -0,0 +1,39 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+	int n,c=0;
+    
+	cin>>n;
+    
+	char a[n];
+    
+	for(int i=0;i<n;i++){
+        
+		cin>>a[i];
+    
+	}
+    
+	sort(a,a+n);
+    
+	if(n<=26){
+        
+		for(int i=0;i<n-1;i++){
+            
+			if(a[i]==a[i+1])
+                
+				c++;
+        
+		}
+         
+		cout<<c<<endl;
+    
+	}
+   
+	 else
+        
+		cout<<-1<<endl;
+
+}
