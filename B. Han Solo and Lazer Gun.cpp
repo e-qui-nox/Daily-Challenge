eee8af72bbdb9deb6a932b71d647e7111797114b
e-qui-nox <yamuna.pixie@gmail.com>
2018-07-26 22:58:58 +0530

    Codeforces

---
+++ B. Han Solo and Lazer Gun.cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	double x0,y0, xi,yi,c=0;
+	set<double>s;
+	cin>>n>>x0>>y0;
+	for(int i=0;i<n;i++){
+        cin>>xi>>yi;
+        if(yi!=y0)
+            s.insert((double)(xi-x0)/(yi-y0));
+        else
+            c=1;
+	}
+	cout<<c+s.size();
+	return 0;
+}
