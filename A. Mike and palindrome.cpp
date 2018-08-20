3b3541f5700e47ddbea6191e67db59960ea9ea10
e-qui-nox <yamuna.pixie@gmail.com>
2018-08-20 23:09:38 +0530

    Codeforces

---
+++ A. Mike and palindrome.cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    int len,cnt=0;
+    cin>>s;
+    len=s.size();
+    for(int i=0;i<len/2;i++)
+    {
+        if(s[i]!=s[len-i-1])
+            cnt++;
+    }
+    if(cnt==1||(cnt<=1&&(len%2!=0)))
+        cout<<"YES";
+    else
+       cout<<"NO";
+    return 0;
+}
