0b70f6ba44bcd530c5a536c8c377a7eab3bed3e3
e-qui-nox <yamuna.pixie@gmail.com>
2018-08-30 00:44:34 +0530

    Codeforces

---
+++ 3-palindrome.cpp
@@ -0,0 +1,17 @@
+﻿
+class Solution {
+public:
+    vector<int> twoSum(vector<int> nums, int t) {
+        vector<int>tmp;
+        for(int i = 0; i < nums.size()-1; i++){
+            for(int j = i+1; j < nums.size(); j++){
+                if(nums[i] + nums[j] == t){
+                    tmp.push_back(i);
+                    tmp.push_back(j);
+                    return tmp;
+                }
+            }
+        }
+       return tmp; 
+    }
+};
