2327b1df51e5d750a485b542436768f69c2bf23d
e-qui-nox <yamuna.pixie@gmail.com>
2018-09-13 04:25:10 +0530

    Codeforces

---
+++ The Eternal Immortality.cpp
@@ -0,0 +1,20 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+using namespace std ;
+ll a,b,c;
+int main ()
+{
+    cin>>a>>b;
+    if(b-a>=10){
+        cout<<0;
+        return 0;
+    }
+    c=1;
+    for(ll i=b;i>a;i--)
+    {
+	    c*=i;
+	    c%=10;
+    }
+    cout<<c;
+    return 0;
+}
