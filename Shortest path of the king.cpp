ca149a7d31c82b799ba7db2b20326273c2ac5a2e
e-qui-nox <yamuna.pixie@gmail.com>
2018-09-12 20:29:59 +0530

    Codeforces

---
+++ Shortest path of the king.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char x,y,a,b;
+    cin>>x>>y;
+    cin>>a>>b;
+    cout<<max(abs(x-a),abs(y-b))<<endl;
+    while (x!=a||y!=b)
+    {
+        if(x<a){cout<<"R";x++;}
+        if(x>a){cout<<"L";x--;}
+        if(y<b){cout<<"U";y++;}
+        if(y>b){cout<<"D";y--;}
+        cout<<endl;
+    }
+}
