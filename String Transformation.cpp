1114f9061901a0a05618a782da712be2b7750b6c
e-qui-nox <yamuna.pixie@gmail.com>
2018-09-22 02:34:14 +0530

    Codeforces

---
+++ String Transformation.cpp
@@ -0,0 +1,19 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char s[100011];
+	cin>>s;
+	char t = 'a';
+	for (int i=0; s[i]; ++i) {
+		if (t > 'z') 
+		    break;
+		if (s[i] <= t) 
+		    s[i] = t++;
+	}
+	if(t>'z')
+	    cout<<s;
+	else
+	    cout<<-1;
+	return 0;
+}
